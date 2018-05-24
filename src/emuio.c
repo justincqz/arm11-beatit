71c6d1ac9266fd8f4050ce9e08f831eb211c54b3
Yoram Boccia <yb4817@ac.ic.uk>
2018-05-24 13:04:22 +0100

    Create emuio.c and correct convert function

---
+++ src/emuio.c
@@ -0,0 +1,68 @@
+//
+// Created by Yoram Boccia on 24/05/2018.
+//
+
+#include <stdio.h>
+#include <math.h>
+#include "emuio.h"
+
+int read(char *fileName, int *address) {
+
+    FILE *file;
+
+    char *buffer;
+    unsigned long lengthFile;
+
+    char *buffer;
+    unsigned long fileLen;
+
+    file = fopen(fileName, "r");
+
+    if (!file) {
+        fprintf(stderr, "Unable to open file %s", fileName);
+        return 0;
+    }
+
+    fseek(file, 0, SEEK_END);
+    fileLen = ftell(file);
+    fseek(file, 0, SEEK_SET);
+
+    buffer = (char *) malloc(fileLen + 1);
+
+    if (!buffer) {
+        fprintf(stderr, "Memory error!");
+        fclose(file);
+        return 0;
+    }
+
+    fread(buffer, fileLen, 1, file);
+    fclose(file);
+
+    convert(buffer, address);
+
+    free(buffer);
+
+}
+
+int convert(char *buffer, int *address) {
+    int x = 0;
+    int y = 0;
+
+
+    while (buffer[y] != EOF) {
+        int decimal = 0;
+        int power = 31;
+        for (int i = 0; i < INSTRUCTION_LENGTH; ++i) {
+            if (buffer[y] == ONE) {
+                decimal += pow(2, power);
+            }
+            y++;
+            power--;
+        }
+        address[x] = decimal;
+        x++;
+    }
+
+    return 0;
+}
+
--- src/emuio.h
+++ src/emuio.h
@@ -2,66 +2,15 @@
 // Created by Yoram Boccia on 24/05/2018.
 //
 
+int const INSTRUCTION_LENGTH = 32;
+char const ONE = '\001';
+char const EOF = '\0';
+
 //instruction from file to address
-int read(char *fileName, int *address) {
+int read(char *fileName, int *address);
 
-    FILE *file;
+//helper function to convert binary to decimal and save in the address
+int convert(char *buffer, int *address);
 
-    char *buffer;
-    unsigned long lengthFile;
-
-    char *buffer;
-    unsigned long fileLen;
-
-    file = fopen(fileName, "r");
-
-    if (!file) {
-        fprintf(stderr, "Unable to open file %s", fileName);
-        return 0;
-    }
-
-    fseek(file, 0, SEEK_END);
-    fileLen = ftell(file);
-    fseek(file, 0, SEEK_SET);
-
-    buffer = (char *) malloc(fileLen + 1);
-
-    if (!buffer) {
-        fprintf(stderr, "Memory error!");
-        fclose(file);
-        return 0;
-    }
-
-    fread(buffer, fileLen, 1, file);
-    fclose(file);
-
-    char *array;
-
-    convert(buffer, address);
-
-    free(buffer);
-
-}
-
-int convert(char *buffer, int *address) {
-    int x = 0;
-    int y = 0;
-
-    int decimal = 1;
-
-    while (buffer[y] != '\0') {
-        int pow = 0;
-        for (int i = 0; i < 32; ++i) {
-            if (buffer[y] == '\001') {
-                decimal = decimal << pow;
-            }
-            y++;
-            pow++;
-        }
-        address[x] = decimal;
-        x++;
-    }
-}
-
-//write(storage)
+//write(Storage storage)
 
