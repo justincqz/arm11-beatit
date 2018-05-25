bb9b193d2d9b43a5ac37ceeae0d56550fcce1c5f
Yoram Boccia <yb4817@ac.ic.uk>
2018-05-25 13:53:28 +0100

    Improved the decoding functions

--- src/emulator/emudecode.c
+++ src/emulator/emudecode.c
@@ -30,11 +30,23 @@ void decode_data_processing(int32_t code, Instruction_t *ins) {
 
     decode_cond_instruction(code, ins);
     ins->opcode = extract_code(code, 21, 24);
+    ins->i = extract_code(code, 25, 25);
     ins->s = extract_code(code, 20, 20);
     ins->rn = extract_code(code, 16, 19);
     ins->rd = extract_code(code, 12, 15);
 
-
+    if (ins->i) {
+        ins->rotate = extract_code(code, 8, 11);
+        ins->imm = extract_code(code, 0, 7);
+    } else {
+        if (extract_code(code, 4,4)) {
+            ins->rs = extract_code(code, 8, 11);
+        } else {
+            ins->shift_constant = extract_code(code, 7, 11);
+        }
+        ins->shift_type = (Shift_Type) extract_code(code, 5, 6);
+        ins->rm = extract_code(code, 0, 3);
+    }
 }
 
 void decode_multiply(int32_t code, Instruction_t *ins){
@@ -61,11 +73,18 @@ void decode_single_data_transfer(int32_t code, Instruction_t *ins){
     ins->rn = extract_code(code, 16, 19);
     ins->rd = extract_code(code, 12, 15);
 
+    if (ins->i) {
+        ins->rs = extract_code(code, 0, 11);
+    } else {
+        ins->imm = extract_code(code, 0, 11);
+    }
+
 }
 
 void decode_branch(int32_t code, Instruction_t *ins){
     assert(ins->instruction_type == BRANCH);
 
     decode_cond_instruction(code, ins);
+    ins->imm = extract_code(code, 0, 23);
 
 }
