695b8b70f3f45fc3b75305cd587f18d12da68ec5
Halite Abudureyimu <ha4017@point46.doc.ic.ac.uk>
2018-06-09 17:09:26 +0100

    really cleaned emulator

--- src/emulator/emuio.c
+++ src/emulator/emuio.c
@@ -8,65 +8,57 @@
 
 
 void emuread(char *fileName, State_t *state) {
-
-    FILE *file;
-	size_t lengthFile;
-    file = fopen(fileName, "rb");
-    assert(file != NULL);
-
-    if (!file) {
-        fprintf(stderr, "Unable to open file %s", fileName);
-        return;
-    }
-
-    fseek(file, 0, SEEK_END);
-    lengthFile = (unsigned long) ftell(file);
-    fseek(file, 0, SEEK_SET);
-	
-	//printf("length of file is %ld\n", lengthFile);
-	size_t size = lengthFile/INSTRUCTION_BYTE;
-	assert(size <= MEMORY_SIZE);
-	state->instructions_size = size;
-
-	for (int i = 0; i < size; i++) {
-		fread(state->storage->mem+i, sizeof(uint32_t),1,file);	
-	}
-
-	fclose(file);
+  FILE *file;
+  size_t lengthFile;
+  file = fopen(fileName, "rb");
+  assert(file != NULL);
+  if (!file) {
+    fprintf(stderr, "Unable to open file %s", fileName);
     return;
+  }
+
+  fseek(file, 0, SEEK_END);
+  lengthFile = (unsigned long) ftell(file);
+  fseek(file, 0, SEEK_SET);
+  
+  //printf("length of file is %ld\n", lengthFile);
+  size_t size = lengthFile/INSTRUCTION_BYTE;
+  assert(size <= MEMORY_SIZE);
+  state->instructions_size = size;
+
+  for (int i = 0; i < size; i++) {
+    fread(state->storage->mem+i, sizeof(uint32_t),1,file);  
+  }
+
+  fclose(file);
+  return;
 }
 
 void emuwrite(Storage_t *storage) {
-
-    assert(storage != NULL);
-
-    printf("Registers:\n");
-
-    for (int i = 0; i < 13; i++) {
-        printf("$%-3d", i);
-        printf(": %10d (0x%08x)\n", storage->reg[i], storage->reg[i]);
-    }
-
-    printf("PC  ");
-    printf(": %10d (0x%08x)\n", storage->reg[PC_REG], storage->reg[PC_REG]);
+  assert(storage != NULL);
+  printf("Registers:\n");
+  for (int i = 0; i < 13; i++) {
+    printf("$%-3d", i);
+    printf(": %10d (0x%08x)\n", storage->reg[i], storage->reg[i]);
+  }
+  printf("PC  ");
+  printf(": %10d (0x%08x)\n", storage->reg[PC_REG], storage->reg[PC_REG]);
 
 //  PART I: initialize the CPSR to 0
-    printf("CPSR");
-    printf(": %10d (0x%08x)\n", storage->reg[CPSR_REG], storage->reg[CPSR_REG]);
-
-    printf("Non-zero memory:\n");
-    for (int i = 0; i < MEMORY_SIZE; i ++) {
-        if ( *(storage->mem + i) != 0) {
-			uint32_t value = *(storage->mem+i);
-			uint32_t t = 4;
-
-            printf("0x%08x: 0x", i*4);;
-			while(t--) {
-				printf("%02x", value & ((1<<8) - 1));
-				value >>= 8;
-			}
-			printf("\n");
-        }
+  printf("CPSR");
+  printf(": %10d (0x%08x)\n", storage->reg[CPSR_REG], storage->reg[CPSR_REG]);
+  printf("Non-zero memory:\n");
+  for (int i = 0; i < MEMORY_SIZE; i ++) {
+    if ( *(storage->mem + i) != 0) {
+    uint32_t value = *(storage->mem+i);
+    uint32_t t = 4;
+    printf("0x%08x: 0x", i*4);;
+    while(t--) {
+      printf("%02x", value & ((1<<8) - 1));
+      value >>= 8;
     }
+    printf("\n");
+    }
+  }
 }
 
